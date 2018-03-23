488ea2e9f122da95f0d98539031a7a1fe2ac8505
Andres Galvez <pocyxDesigner@hotmail.com>
2018-03-23 10:16:08 +0100

    product service (get,insert,update,delete)

--- angular-crud-firebase/src/app/services/product.service.ts
+++ angular-crud-firebase/src/app/services/product.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList }  from 'angularfire2/database';
+import { Product } from '../models/product';
 
 @Injectable()
 export class ProductService {
+  productList: AngularFireList<any>;
+  selectProduct: Product = new Product();
 
-  constructor() { }
+  constructor(private firebase: AngularFireDatabase) { }
+
+  getProduct(){
+    return this.productList = this.firebase.list('products');
+  }
+
+  insertProduct(product: Product){
+    this.productList.push({
+      name: product.name,
+      category: product.category,
+      location: product.location,
+      price: product.price
+    });
+  }
+
+  updateProduct(product: Product){
+    this.productList.update(product.$key, {
+        name: product.name,
+        category: product.category,
+        location: product.location,
+        price: product.price
+    });
+  }
+
+  deleteProduct($key: string){
+    this.productList.remove($key);
+  }
 
 }
